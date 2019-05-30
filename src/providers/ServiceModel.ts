a90097de87159dc0bce39b12430d612ee51bec99
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-30 14:04:58 +0300

    Steal common Model methods from JorgenVatle/atshop-api.

--- src/providers/ServiceModel.ts
+++ src/providers/ServiceModel.ts
@@ -1,5 +1,9 @@
+import { Application, Id, Paginated, Params } from '@feathersjs/feathers';
 import ModelDocument from '../interfaces/ModelDocument';
-import { Application } from '@feathersjs/feathers';
+import { NotFound } from '@feathersjs/errors';
+
+export type ServiceModelClass<T> = { new(data: ModelDocument): T };
+export type AsyncKey = Id | Promise<Id>;
 
 interface ServiceModel extends ModelDocument {
     entry: ModelDocument;
@@ -68,6 +72,91 @@ abstract class ServiceModel {
 
         this.entry = entry;
         return this;
+    }
+
+    /**
+     * Query the database to re-fetch data for this model.
+     */
+    public async _renew(data?: ModelDocument) {
+        // @ts-ignore
+        this._renewEntry(data || await this.constructor.service.get(this._id));
+    }
+
+    /**
+     * Merge and store the ServiceModel's in-database data with the given data.
+     */
+    public async patch(data: any): Promise<void> {
+        // @ts-ignore
+        const service = this.constructor.service;
+
+        await this._renew(await service.patch(this._id, data));
+    }
+
+    /**
+     * Register has-many relationship for the current model.
+     */
+    protected hasMany(model: typeof ServiceServiceModel, foreignKey: string, localKey = this.entry._id) {
+        const query: Params['query'] = {};
+        query[foreignKey] = localKey;
+
+        return new PaginatedServiceModel(model, query);
+    }
+
+    /**
+     * Registers a relationship between the current model instance and the given ServiceModel instance.
+     */
+    protected belongsTo<T extends ServiceModel>(model: ServiceModelClass<T>, foreignKey: AsyncKey): Promise<T> {
+        // @ts-ignore
+        return model.get(foreignKey);
+    }
+
+    /**
+     * Find a list of entries from the current service.
+     */
+    public static _find(query: Params['query']): Promise<Paginated<ModelDocument>> {
+        return this.service.find({ query });
+    }
+
+    /**
+     * Update the models on the current collection.
+     */
+    public static _patch(id: string | null, data: any, params?: Params) {
+        return this.service.patch(id, data, params);
+    }
+
+    /**
+     * Find and format a list of entries from the current service.
+     */
+    public static async find(query: Params) {
+        return new PaginatedServiceModel(this, query);
+    }
+
+    /**
+     * Fetch a single entry from the current service.
+     */
+    public static async get<T extends ServiceModel>(this: ServiceModelClass<T>, id: AsyncKey, query?: Params): Promise<T> {
+        const self = <typeof ServiceModel><unknown>this;
+        const result = await self.service.get(await id, query);
+
+        if (!result) {
+            throw new NotFound(`The requested entry could not be located!`, {
+                id,
+                query,
+                service: self.servicePath,
+            });
+        }
+
+        return new this(result);
+    }
+
+    /**
+     * Insert an entry into the database.
+     */
+    public static async create(data: any, params?: Params) {
+        // @ts-ignore
+        return new this(
+            await this.service.create(data, params)
+        );
     }
 }
 

c633cea5c7d95084b4807cd6ee22fd95ab29fab9
Jørgen Vatle <Jorgen@WcAServices.net>
2019-05-30 14:06:23 +0300

    Steal PaginatedServiceModel from JorgenVatle/atshop-api

---
+++ src/providers/PaginatedServiceModel.ts
@@ -0,0 +1,118 @@
+import { Paginated, Params } from '@feathersjs/feathers';
+import Model from './ServiceModel';
+
+export default class PaginatedModel {
+
+    /**
+     * Model to be paginated
+     */
+    private readonly model: typeof Model;
+
+    /**
+     * Service query.
+     */
+    private readonly query: Params['query'];
+
+    /**
+     * Cached pagination result.
+     */
+    private _result: Paginated<Model> = { total: -1, limit: -1,  skip: -1, data: [] };
+
+    /**
+     * Paginated model constructor.
+     *
+     * @param model
+     * @param query
+     */
+    constructor(model: typeof Model, query: Params['query']) {
+        this.model = model;
+        this.query = query;
+    }
+
+    /**
+     * Format a raw paginated response into a model"-ified" response.
+     *
+     * @param result
+     */
+    private formatResult(result: Paginated<any>) {
+        return {
+            ...result,
+            data: result.data.map((document: any) => {
+                // @ts-ignore
+                return new this.model(document);
+            }),
+        }
+    }
+
+    /**
+     * Fetch the pagination result.
+     */
+    public async fetch(query: Params['query'] = {}) {
+        if (this._result.total !== -1) {
+            return this._result;
+        }
+
+        const result = await this.model._find({ ...query, ...this.query });
+
+        return this._result = this.formatResult(result)
+    }
+
+    /**
+     * Fetch a single entry from the paginated model.
+     *
+     * @param query
+     */
+    public async fetchOne(query: Params['query'] = {}) {
+        const result = await this.fetch(query);
+
+        if (result.data.length) {
+            return result.data[0];
+        }
+
+        return null;
+    }
+
+    /**
+     * Further dig through the pagination with additional search query.
+     *
+     * @param query
+     */
+    public find(query?: Params['query']) {
+        return new PaginatedModel(this.model, { ...query, ...this.query, });
+    }
+
+    /**
+     * Patch all matched models.
+     */
+    public patch(data: any, params?: Params) {
+        return this.model._patch(null, data, {
+            ...params,
+            query: {
+                ...(params && params.query),
+                ...this.query,
+            }
+        });
+    }
+
+    /**
+     * Fetch result count.
+     */
+    public async count(): Promise<number> {
+        const result = await this.fetch({ $limit: 0 });
+        return result.total;
+    }
+
+    /**
+     * Create a new model entry using the current query (likely relationship data) as the document base.
+     *
+     * @param data
+     * @param query
+     */
+    public create(data: any, query?: Params['query']) {
+        return this.model.create({
+            ...this.query,
+            ...data,
+        }, query)
+    }
+
+}

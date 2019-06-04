979ddb018c2c9d93ff4a43bb42409eb688e12c90
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-04 14:34:11 +0300

    Export ShopModel in package index.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { config } from './utility/Service';
+import ShopModel from './models/ShopModel';
 
 /**
  * Setup Feathers Service Models.
  */
 export const SetupServiceModels = config;
+
+export {
+    ShopModel,
+}

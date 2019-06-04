1f9fc4e46533a096e50721e953a65666e308f16b
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-04 15:12:21 +0300

    Assert that Models can interact with the current Feathers service.

--- tests/service.test.ts
+++ tests/service.test.ts
@@ -1,6 +1,7 @@
 import { SetupServiceModels } from '../src';
 import Client from './FeathersClient';
 import ShopModel from '../src/models/ShopModel';
+import { NotFound } from '@feathersjs/errors';
 
 class BaseModel  {
     static get testGetter() {
@@ -8,12 +9,16 @@ class BaseModel  {
     }
 }
 
-test('can specify a custom BaseModel', () => {
-     SetupServiceModels({
-         app: Client,
-         baseModel: BaseModel,
-     });
+SetupServiceModels({
+    app: Client,
+    baseModel: BaseModel,
+});
 
+test('can use a custom BaseModel', () => {
      // @ts-ignore
      expect(ShopModel.testGetter).toBe('test');
+});
+
+test('can interact with the Feathers server', async () => {
+    await expect(ShopModel.get('some-id')).rejects.toBeInstanceOf(NotFound);
 });

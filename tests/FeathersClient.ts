acc503b3bf74362ff00cf37cccfa499997218506
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-04 14:58:20 +0300

    Add base FeathersClient for tests.

---
+++ tests/FeathersClient.ts
@@ -0,0 +1,10 @@
+import Feathers from '@feathersjs/feathers';
+import FeathersRest from '@feathersjs/rest-client';
+import Axios from 'axios';
+
+const Client = Feathers();
+const RestClient = FeathersRest('http://localhost:30303');
+
+Client.configure(RestClient.axios(Axios));
+
+export default Client;

6edf7d90ef49da2e99d2f468e98fd66cb40aaecf
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-11 19:26:19 +0300

    Add service path to CustomerBlacklistModel.

--- src/models/CustomerBlacklistModel.ts
+++ src/models/CustomerBlacklistModel.ts
@@ -2,6 +2,11 @@ import ServiceModel from '../providers/ServiceModel';
 
 class CustomerBlacklistModel extends ServiceModel {
 
+    /**
+     * Service path for Customer Blacklist Model.
+     */
+    public static readonly servicePath = '/shop/blacklist';
+
 }
 
 export default CustomerBlacklistModel;

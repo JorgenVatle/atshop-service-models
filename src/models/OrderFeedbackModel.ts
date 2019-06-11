d2cde49ecec7c6ee57fdb05f7b743599f1cffcbd
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-12 00:26:14 +0300

    Add OrderFeedbackModel.

---
+++ src/models/OrderFeedbackModel.ts
@@ -0,0 +1,25 @@
+import ServiceModel from '../providers/ServiceModel';
+import OrderModel from './OrderModel';
+import OrderFeedbackDocument from '../interfaces/OrderFeedbackDocument';
+
+interface OrderFeedbackModel extends OrderFeedbackDocument {
+    entry: OrderFeedbackDocument;
+}
+
+class OrderFeedbackModel extends ServiceModel {
+
+    /**
+     * Order Feedback Model service path.
+     */
+    public static readonly servicePath = '/shop/order/feedback';
+
+    /**
+     * Order feedback belongs to an order.
+     */
+    public get order() {
+        return this.belongsTo(OrderModel, this.orderId);
+    }
+
+}
+
+export default OrderFeedbackModel;

90ded8f1432eb4a30a6658f55f708c26c6258018
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-11 19:11:55 +0300

    Add service path to ProductStockModel.

--- src/models/ProductStockModel.ts
+++ src/models/ProductStockModel.ts
@@ -2,6 +2,11 @@ import ServiceModel from '../providers/ServiceModel';
 
 class ProductStockModel extends ServiceModel {
 
+    /**
+     * Service path for Product Stock Model
+     */
+    public static readonly servicePath = '/shop/product/stock'
+
 }
 
 export default ProductStockModel;

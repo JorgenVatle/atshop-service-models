b78c98f9d0d3dcfdea4a281244a1d049450efed8
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-12 00:29:28 +0300

    Add UserModel.

---
+++ src/models/UserModel.ts
@@ -0,0 +1,15 @@
+import ServiceModel from '../providers/ServiceModel';
+import { UserDocument } from '../interfaces/UserDocument';
+
+interface UserModel extends UserDocument {
+    entry: UserDocument;
+}
+
+class UserModel extends ServiceModel {
+
+    /**
+     * User Model service path.
+     */
+    public static readonly servicePath = '/users';
+
+}

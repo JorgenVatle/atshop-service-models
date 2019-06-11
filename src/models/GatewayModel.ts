bfe8ea4a127b781caaa00febf0b8e06a10d9aae2
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-11 19:28:56 +0300

    Add service path to GatewayModel.

--- src/models/GatewayModel.ts
+++ src/models/GatewayModel.ts
@@ -2,6 +2,11 @@ import ServiceModel from '../providers/ServiceModel';
 
 class GatewayModel extends ServiceModel {
 
+    /**
+     * Gateway Model service path.
+     */
+    public static readonly servicePath = '/shop/gateways';
+
 }
 
 export default GatewayModel;

a33f47f93514e366a4bfd7436d2a941c0f4b122a
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-11 19:59:13 +0300

    Add IpnModel class foundation.

---
+++ src/models/IpnModel.ts
@@ -0,0 +1,12 @@
+import ServiceModel from '../providers/ServiceModel';
+
+class IpnModel extends ServiceModel {
+
+    /**
+     * Ipn Model service path.
+     */
+    public static readonly servicePath = '/shop/ipn'
+
+}
+
+export default IpnModel;

8fa16f3c7a9218311a3a595f4e79d2d8a694e5c3
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-11 07:16:31 +0300

    Assert that ShopModel has many products.

--- tests/service.test.ts
+++ tests/service.test.ts
@@ -1,18 +1,33 @@
 import { SetupServiceModels } from '../src';
 import Client from './FeathersClient';
 import ShopModel from '../src/models/ShopModel';
+import ProductModel from '../src/models/ProductModel';
 
 SetupServiceModels({
     app: Client,
 });
 
+/**
+ * Always available sandbox test shop.
+ */
+let testShop: ShopModel;
+beforeAll(async () => {
+    testShop = await ShopModel.get('ZBAWZE4LzB4RoguGY')
+});
+
 describe('ShopModel', () => {
     test('can get() a shop by ID', async () => {
-        await expect(ShopModel.get('ZBAWZE4LzB4RoguGY')).resolves.toBeDefined();
+        await expect(ShopModel.get(testShop._id)).resolves.toBeDefined();
     });
 
     test('can find() a shop by domain', async () => {
-        const shops = await ShopModel.find({ domain: 'test-shop' }).fetch();
+        const shops = await ShopModel.find({ domain: testShop.domain }).fetch();
         expect(shops.total).toBeGreaterThan(0);
+    });
+
+    test('has many products', async () => {
+        const products = await testShop.products.fetch();
+        expect(products.total).toBeDefined();
+        expect(products.data[0]).toBeInstanceOf(ProductModel);
     });
 });

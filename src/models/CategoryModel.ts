036e1f31a0b7cf279c8e5fec17381129ee85d440
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-11 23:59:44 +0300

    Add CategoryModel.

---
+++ src/models/CategoryModel.ts
@@ -0,0 +1,15 @@
+import ServiceModel from '../providers/ServiceModel';
+import CategoryDocument from '../interfaces/CategoryDocument';
+
+interface CategoryModel extends CategoryDocument {
+    entry: CategoryDocument;
+}
+
+class CategoryModel extends ServiceModel {
+
+    /**
+     * Service path for Category Model.
+     */
+    public static readonly servicePath = '/shop/categories';
+
+}

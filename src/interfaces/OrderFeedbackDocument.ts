db16623f430164a6bca3b30b9ef6f4c17ed6ab17
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-13 01:43:40 +0300

    Document OrderFeedbackDocument properties.

--- src/interfaces/OrderFeedbackDocument.ts
+++ src/interfaces/OrderFeedbackDocument.ts
@@ -1,10 +1,29 @@
 import ModelDocument from './ModelDocument';
 
 export default interface OrderFeedbackDocument extends ModelDocument {
+    /**
+     * Customer provided feedback.
+     */
     feedback: {
+        /**
+         * Customer message
+         * @note optional
+         */
         message: string;
+
+        /**
+         * Whether the user was satisfied or dissatisfied with the order.
+         */
         rating: 'satisfied' | 'dissatisfied';
     }
+
+    /**
+     * ID of the order this feedback belongs to.
+     */
     orderId: string;
+
+    /**
+     * ID of the product ordered.
+     */
     productId: string;
 }

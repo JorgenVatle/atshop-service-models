85a7ff48bdf803f021c6aa682cfa873736161095
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-13 01:26:24 +0300

    Document ModelDocument properties.

--- src/interfaces/ModelDocument.ts
+++ src/interfaces/ModelDocument.ts
@@ -1,6 +1,16 @@
 export default interface ModelDocument {
+    /**
+     * Unique identifier for the current document.
+     */
     _id: string;
 
+    /**
+     * Document creation date.
+     */
     createdAt: Date;
+
+    /**
+     * Date of the last document update.
+     */
     updatedAt: Date;
 }

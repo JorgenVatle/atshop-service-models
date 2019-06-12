710e81a21b3eff0b709c89e84e1ea8649d896904
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-13 02:06:45 +0300

    Document properties of ProductStockDocument interface.

--- src/interfaces/ProductStockDocument.ts
+++ src/interfaces/ProductStockDocument.ts
@@ -1,5 +1,10 @@
 import ModelDocument from './ModelDocument';
 
 export default interface ProductStockDocument extends ModelDocument {
+
+    /**
+     * String that is given to the customer on payment completion.
+     */
     entry: string;
+
 }

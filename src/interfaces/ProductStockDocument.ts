cda42dab74e2c87fd4716211174b5635e1b99e72
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-13 02:08:37 +0300

    Add missing properties of ProductStockDocument.

--- src/interfaces/ProductStockDocument.ts
+++ src/interfaces/ProductStockDocument.ts
@@ -7,4 +7,24 @@ export default interface ProductStockDocument extends ModelDocument {
      */
     entry: string;
 
+    /**
+     * ID of the product this stock entry belongs to.
+     */
+    productId: string;
+
+    /**
+     * ID of the order this stock entry belongs to. (If sold!)
+     */
+    orderId?: string;
+
+    /**
+     * Whether or not the current stock entry has been sold.
+     */
+    sold: boolean;
+
+    /**
+     * Whether or not the current stock entry is up for sale.
+     */
+    forSale: boolean;
+
 }

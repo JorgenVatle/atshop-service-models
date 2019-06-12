725fae4bf10faa6018e8b73a9b66a07afc7c85a0
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-13 01:42:28 +0300

    Document OrderEventDocument types.

--- src/interfaces/OrderEventDocument.ts
+++ src/interfaces/OrderEventDocument.ts
@@ -1,17 +1,43 @@
 import ModelDocument from './ModelDocument';
 
+/**
+ * Order event name.
+ */
 export type OrderEventName = string;
+
+/**
+ * Order event metadata.
+ */
 export type OrderEventMetadata = {
     [s: string]: any,
 }
 
 export interface BaseMetadata {
+    /**
+     * Human readable heading for the current event.
+     */
     title: string;
+
+    /**
+     * Event level for use in determining a semantic highlight level in the administration interface.
+     */
     level: 'success' | 'warning' | 'default' | 'danger';
 }
 
 export default interface OrderEventDocument extends ModelDocument {
+    /**
+     * ID of the order this event belongs to.
+     */
     orderId: string;
+
+    /**
+     * Computer-readable event title.
+     * @note Used as a fallback if we can't generate a human readable title for the current event.
+     */
     name: OrderEventName;
+
+    /**
+     * Event metadata.
+     */
     metadata: OrderEventMetadata;
 }

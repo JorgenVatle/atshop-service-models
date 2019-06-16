45d972615618f159e6904783579a6847a931d98f
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-16 07:43:19 +0300

    Allow configurable model overrides in Service.ts.

--- src/utility/Service.ts
+++ src/utility/Service.ts
@@ -1,11 +1,35 @@
 import { Application } from '@feathersjs/feathers';
+import OrderModel from '../models/OrderModel';
+import CategoryModel from '../models/CategoryModel';
+import CustomerBlacklistModel from '../models/CustomerBlacklistModel';
+import GatewayModel from '../models/GatewayModel';
+import IpnModel from '../models/IpnModel';
+import OrderEventModel from '../models/OrderEventModel';
+import OrderFeedbackModel from '../models/OrderFeedbackModel';
+import ProductModel from '../models/ProductModel';
+import ProductStockModel from '../models/ProductStockModel';
+import ShopModel from '../models/ShopModel';
+import UserModel from '../models/UserModel';
 
 interface ConfigOptions {
     app: Application,
     frontend?: {
         host?: string,
         protocol?: 'http' | 'https',
-    }
+    },
+    models?: Partial<{
+        CategoryModel: typeof CategoryModel,
+        CustomerBlacklistModel: typeof CustomerBlacklistModel,
+        GatewayModel: typeof GatewayModel,
+        IpnModel: typeof IpnModel,
+        OrderEventModel: typeof OrderEventModel,
+        OrderFeedbackModel: typeof OrderFeedbackModel,
+        OrderModel: typeof OrderModel,
+        ProductModel: typeof ProductModel,
+        ProductStockModel: typeof ProductStockModel,
+        ShopModel: typeof ShopModel,
+        UserModel: typeof UserModel,
+    }>,
 }
 
 /**
@@ -22,5 +46,11 @@ export const config = (options: ConfigOptions) => {
         host: 'atshop.io',
         protocol: 'https',
         ...options.frontend,
-    })
+    });
+
+    if (options.models) {
+        Object.entries(options.models).forEach(([modelName, model]) => {
+            App.set(`atshop-service-models.model.${modelName}`, model);
+        });
+    }
 };

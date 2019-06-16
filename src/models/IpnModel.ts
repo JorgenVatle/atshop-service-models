7f436c50faa82ce151aa74745a8ce235821c9bff
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-16 13:25:52 +0300

    Merge IpnDocument interface with IpnModel.

--- src/models/IpnModel.ts
+++ src/models/IpnModel.ts
@@ -1,12 +1,17 @@
 import ServiceModel from '../providers/ServiceModel';
+import IpnDocument from '../interfaces/IpnDocument';
 
 class IpnModel extends ServiceModel {
 
     /**
      * Ipn Model service path.
      */
-    public static readonly servicePath = '/shop/ipn'
+    public static readonly servicePath = '/shop/ipn';
 
+}
+
+interface IpnModel extends IpnDocument{
+    entry: IpnDocument;
 }
 
 export default IpnModel;

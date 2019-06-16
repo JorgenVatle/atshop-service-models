89c98ae1250ab06c26f19422d5fc1d190bff4300
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-16 17:03:32 +0300

    Add JSDoc to Omit<> TS utility.

--- src/utility/TS.ts
+++ src/utility/TS.ts
@@ -3,4 +3,7 @@
  */
 export type KeyValue = { [key: string]: any };
 
+/**
+ * Omit the given keys from the given interface.
+ */
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>

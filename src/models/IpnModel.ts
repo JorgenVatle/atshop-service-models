4579439075db38b3ba0ab612a4456adea06c45f3
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-16 13:26:49 +0300

    Add humanName() getter to IpnModel.

--- src/models/IpnModel.ts
+++ src/models/IpnModel.ts
@@ -1,4 +1,5 @@
 import ServiceModel from '../providers/ServiceModel';
+import { startCase } from 'lodash';
 import IpnDocument from '../interfaces/IpnDocument';
 
 class IpnModel extends ServiceModel {
@@ -7,6 +8,13 @@ class IpnModel extends ServiceModel {
      * Ipn Model service path.
      */
     public static readonly servicePath = '/shop/ipn';
+
+    /**
+     * Human friendly name for the payment gateway associated with this IPN.
+     */
+    public get humanName() {
+        return startCase(this.gateway);
+    }
 
 }
 

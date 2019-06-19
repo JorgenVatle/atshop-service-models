08b95f954c020c1c8ec0f513369b50cb4717662b
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-19 14:26:34 +0300

    Add FeathersFactories to FeathersClient.

--- tests/FeathersClient.ts
+++ tests/FeathersClient.ts
@@ -2,11 +2,13 @@ import Feathers from '@feathersjs/feathers';
 import FeathersRest from '@feathersjs/rest-client';
 import Axios from 'axios';
 import { ATShopServiceModels } from '../src';
+import FeathersFactories from './FeathersFactories';
 
 const Client = Feathers();
 const RestClient = FeathersRest('https://api_sandbox.atshop.io');
 
 Client.configure(RestClient.axios(Axios));
 Client.configure(ATShopServiceModels());
+Client.configure(FeathersFactories);
 
 export default Client;

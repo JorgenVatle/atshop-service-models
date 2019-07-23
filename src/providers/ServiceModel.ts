9c68f622964d22cd56899dd57339b6a6eaefeaa7
Jørgen Vatle <Jorgen@WcAServices.net>
2019-07-23 23:17:46 +0300

    Re-order methods of ServiceModel.

--- src/providers/ServiceModel.ts
+++ src/providers/ServiceModel.ts
@@ -20,145 +20,6 @@ class ServiceModel {
     };
 
     /**
-     * Dynamic accessor for Feathers application.
-     */
-    public get _App() {
-        // @ts-ignore
-        return this.constructor.App;
-    }
-
-    /**
-     * FeathersVuex Model for the current service.
-     */
-    public get vuex() {
-        // @ts-ignore
-       const model = this.constructor.service.FeathersVuexModel;
-
-       if (!model) {
-           console.warn('[atshop-service-models] No FeathersVuexModel available for this service.', this.constructor.name)
-       }
-
-       return model;
-    }
-
-    /**
-     * Feathers service path.
-     */
-    public static servicePath: string;
-
-    /**
-     * Feathers service instance for this model.
-     */
-    public static get service(): Service<any> {
-        return this.App.service(this.servicePath);
-    }
-
-    /**
-     * Model constructor.
-     */
-    public constructor(data: ModelDocument) {
-        this._renewEntry(data);
-    }
-
-    /**
-     * Merge entry data with the current model.
-     */
-    protected _renewEntry(entry: ModelDocument) {
-        this.entry = entry;
-
-        Object.entries(entry).forEach(([key, value]) => {
-            const set = () => { this[key] = value };
-            let descriptorTarget = this;
-            let descriptor;
-
-            while(!descriptor && (descriptorTarget = Object.getPrototypeOf(descriptorTarget))) {
-                descriptor = Object.getOwnPropertyDescriptor(descriptorTarget, key);
-            }
-
-            if (!descriptor) {
-                return set();
-            }
-
-            if (typeof descriptor.set === 'function') {
-                descriptor.set(value);
-                return;
-            }
-
-            if (typeof descriptor.get === 'function') {
-                return;
-            }
-
-            if (typeof descriptor.value === 'function') {
-                return;
-            }
-
-            set();
-        });
-
-        this.entry = entry;
-        return this;
-    }
-
-    /**
-     * Query the database to re-fetch data for this model.
-     */
-    public async _renew(data?: ModelDocument) {
-        // @ts-ignore
-        this._renewEntry(data || await this.constructor.service.get(this._id));
-    }
-
-    /**
-     * Merge and store the ServiceModel's in-database data with the given data.
-     */
-    public async patch(data: any): Promise<void> {
-        // @ts-ignore
-        const service = this.constructor.service;
-
-        await this._renew(await service.patch(this._id, data));
-    }
-
-    /**
-     * Register has-many relationship for the current model.
-     */
-    protected hasMany<T extends typeof ServiceModel>(model: ModelName, foreignKey: string, localKey = this.entry._id) {
-        const query: Params['query'] = {};
-        query[foreignKey] = localKey;
-
-        return new PaginatedServiceModel(this.getModel<T>(model), query);
-    }
-
-    /**
-     * Register a has-one relationship for the current model.
-     */
-    protected hasOne<T extends typeof ServiceModel>(model: ModelName, foreignKey: string, localKey: AsyncKey = this._id) {
-        const query: Params['query'] = {};
-        query[foreignKey] = localKey;
-
-        return new PaginatedServiceModel(this.getModel<T>(model), query).fetchOne();
-    }
-
-    /**
-     * Registers a relationship between the current model instance and the given ServiceModel instance.
-     */
-    protected belongsTo<T extends typeof ServiceModel>(modelName: ModelName, foreignKey: AsyncKey) {
-        return this.getModel<T>(modelName).get(foreignKey);
-    }
-
-    /**
-     * Fetch a model by name.
-     */
-    private getModel<T extends typeof ServiceModel>(modelName: ModelName): T {
-        return this._App.get(`atshop-service-models.model.${modelName}`) || require(`../models/${modelName}`).default;
-    }
-
-    /**
-     * Remove the current model from the database.
-     */
-    public remove() {
-        return this._App.remove(this._id);
-    }
-
-    /**
      * Find a list of entries from the current service.
      */
     public static _find(query: Params['query']): Promise<Paginated<ModelDocument>> {
@@ -206,6 +67,145 @@ class ServiceModel {
             // @ts-ignore
             await this.service.create(data, params)
         );
+    }
+
+    /**
+     * Feathers service path.
+     */
+    public static servicePath: string;
+
+    /**
+     * Feathers service instance for this model.
+     */
+    public static get service(): Service<any> {
+        return this.App.service(this.servicePath);
+    }
+
+    /**
+     * Model constructor.
+     */
+    public constructor(data: ModelDocument) {
+        this._renewEntry(data);
+    }
+
+    /**
+     * Dynamic accessor for Feathers application.
+     */
+    public get _App() {
+        // @ts-ignore
+        return this.constructor.App;
+    }
+
+    /**
+     * FeathersVuex Model for the current service.
+     */
+    public get vuex() {
+        // @ts-ignore
+        const model = this.constructor.service.FeathersVuexModel;
+
+        if (!model) {
+            console.warn('[atshop-service-models] No FeathersVuexModel available for this service.', this.constructor.name)
+        }
+
+        return model;
+    }
+
+    /**
+     * Merge and store the ServiceModel's in-database data with the given data.
+     */
+    public async patch(data: any): Promise<void> {
+        // @ts-ignore
+        const service = this.constructor.service;
+
+        await this._renew(await service.patch(this._id, data));
+    }
+
+    /**
+     * Remove the current model from the database.
+     */
+    public remove() {
+        return this._App.remove(this._id);
+    }
+
+    /**
+     * Query the database to re-fetch data for this model.
+     */
+    public async _renew(data?: ModelDocument) {
+        // @ts-ignore
+        this._renewEntry(data || await this.constructor.service.get(this._id));
+    }
+
+    /**
+     * Register has-many relationship for the current model.
+     */
+    protected hasMany<T extends typeof ServiceModel>(model: ModelName, foreignKey: string, localKey = this.entry._id) {
+        const query: Params['query'] = {};
+        query[foreignKey] = localKey;
+
+        return new PaginatedServiceModel(this.getModel<T>(model), query);
+    }
+
+    /**
+     * Register a has-one relationship for the current model.
+     */
+    protected hasOne<T extends typeof ServiceModel>(model: ModelName, foreignKey: string, localKey: AsyncKey = this._id) {
+        const query: Params['query'] = {};
+        query[foreignKey] = localKey;
+
+        return new PaginatedServiceModel(this.getModel<T>(model), query).fetchOne();
+    }
+
+    /**
+     * Registers a relationship between the current model instance and the given ServiceModel instance.
+     */
+    protected belongsTo<T extends typeof ServiceModel>(modelName: ModelName, foreignKey: AsyncKey) {
+        return this.getModel<T>(modelName).get(foreignKey);
+    }
+
+    /**
+     * Merge entry data with the current model.
+     */
+    protected _renewEntry(entry: ModelDocument) {
+        this.entry = entry;
+
+        Object.entries(entry).forEach(([key, value]) => {
+            const set = () => { this[key] = value };
+            let descriptorTarget = this;
+            let descriptor;
+
+            while(!descriptor && (descriptorTarget = Object.getPrototypeOf(descriptorTarget))) {
+                descriptor = Object.getOwnPropertyDescriptor(descriptorTarget, key);
+            }
+
+            if (!descriptor) {
+                return set();
+            }
+
+            if (typeof descriptor.set === 'function') {
+                descriptor.set(value);
+                return;
+            }
+
+            if (typeof descriptor.get === 'function') {
+                return;
+            }
+
+            if (typeof descriptor.value === 'function') {
+                return;
+            }
+
+            set();
+        });
+
+        this.entry = entry;
+        return this;
+    }
+
+    /**
+     * Fetch a model by name.
+     */
+    private getModel<T extends typeof ServiceModel>(modelName: ModelName): T {
+        return this._App.get(`atshop-service-models.model.${modelName}`) || require(`../models/${modelName}`).default;
     }
 }
 

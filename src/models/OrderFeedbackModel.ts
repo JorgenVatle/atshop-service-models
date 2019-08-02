591238711aa020dc6bba3129cd1061c48e2a43b0
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-02 15:21:26 +0300

    Add static rating() method to OrderFeedbackModel.

--- src/models/OrderFeedbackModel.ts
+++ src/models/OrderFeedbackModel.ts
@@ -24,6 +24,32 @@ class OrderFeedbackModel extends ServiceModel {
         return this.belongsTo<typeof ProductModel>('ProductModel', this.productId);
     }
 
+    /**
+     * Fetch the rating for one or more products.
+     */
+    public static async rating(productIds: string[]) {
+        const feedback = await this.find({
+            query: {
+                productId: {
+                    $in: productIds
+                },
+                $limit: -1,
+            }
+        }).fetch();
+
+        const ratings: number[] = await feedback.data.map((feedback) => feedback.positive ? 1 : 0);
+        let sum = 0;
+
+        if (ratings.length) {
+            sum = ratings.reduce((previous, current) => previous + current);
+        }
+
+        return {
+            count: ratings.length,
+            score: Math.round((sum / ratings.length) * 100) || -1,
+        }
+    }
+
 }
 
 interface OrderFeedbackModel extends OrderFeedbackDocument {

955e0ab9b90712f39afbf217465a65ab0c27bf2a
Jørgen Vatle <Jorgen@WcAServices.net>
2019-10-27 09:51:21 +0200

    Add order() relationship getter to OrderReplacementModel.

--- src/models/OrderReplacementModel.ts
+++ src/models/OrderReplacementModel.ts
@@ -1,5 +1,6 @@
 import ServiceModel from '../providers/ServiceModel';
 import OrderFeedbackDocument from '../interfaces/OrderFeedbackDocument';
+import OrderModel from './OrderModel';
 
 class OrderReplacementModel extends ServiceModel {
 
@@ -7,6 +8,13 @@ class OrderReplacementModel extends ServiceModel {
      * Service path for order replacements.
      */
     public static readonly servicePath = '/shop/order/replacements';
+
+    /**
+     * A replacement belongs to an order.
+     */
+    public get order() {
+        return this.belongsTo<typeof OrderModel>('OrderModel', this.orderId);
+    }
 
 }
 

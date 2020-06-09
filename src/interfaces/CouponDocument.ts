f48d405f6c5971e626a8d6680709ef8b0bb7cd45
Jørgen Vatle <Jorgen@WcAServices.net>
2020-06-09 10:55:46 +0300

    Extend CouponDocument interface with ModelDocument interface.

--- src/interfaces/CouponDocument.ts
+++ src/interfaces/CouponDocument.ts
@@ -1,4 +1,6 @@
-export default interface CouponDocument {
+import ModelDocument from './ModelDocument';
+
+export default interface CouponDocument extends ModelDocument {
     /**
      * ID of the shop this coupon belongs to.
      */

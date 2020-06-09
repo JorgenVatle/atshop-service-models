de0ce95f1fc39cf085ad482532ca3d107da35e8d
Jørgen Vatle <Jorgen@WcAServices.net>
2020-06-09 12:22:36 +0300

    Add CouponModel to Models interface.

--- src/interfaces/Models.ts
+++ src/interfaces/Models.ts
@@ -11,6 +11,7 @@ import UserModel from '../models/UserModel';
 import OrderFeedbackModel from '../models/OrderFeedbackModel';
 import ProductGroupModel from '../models/ProductGroupModel';
 import OrderReplacementModel from '../models/OrderReplacementModel';
+import CouponModel from '../models/CouponModel';
 
 export default interface Models {
     CategoryModel: typeof CategoryModel;
@@ -26,6 +27,7 @@ export default interface Models {
     UserModel: typeof UserModel;
     ProductGroupModel: typeof ProductGroupModel;
     OrderReplacementModel: typeof OrderReplacementModel;
+    CouponModel: typeof CouponModel;
 }
 
 export type ModelName = keyof Models;

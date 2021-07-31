582dcfd3da124dddc0382020f75c3043c4c54db4
Jørgen Vatle <Jorgen@WcAServices.net>
2021-07-31 16:30:14 +0200

    Add currency code type to TS helpers.

--- src/utility/TS.ts
+++ src/utility/TS.ts
@@ -7,3 +7,8 @@ export type KeyValue<T = any> = { [key: string]: T };
  * Omit the given keys from the given interface.
  */
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
+/**
+ * Currency codes supported by ATShop.
+ */
+export type CurrencyCode<Extras extends string = never> = 'USD' | Extras;

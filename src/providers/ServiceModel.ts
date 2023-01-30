5b7c58fede3fb3ac8ebf579adb2f2ba2ffddba37
Jørgen Vatle <Jorgen@WcAServices.net>
2023-01-30 11:53:41 +0100

    Correct this type for static find() method.

--- src/providers/ServiceModel.ts
+++ src/providers/ServiceModel.ts
@@ -36,7 +36,7 @@ class ServiceModel {
     /**
      * Find and format a list of entries from the current service.
      */
-    public static find(query: Params) {
+    public static find<Self extends StaticModel>(this: Self, query: Params) {
         return new PaginatedServiceModel(this, query);
     }
 

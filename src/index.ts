cdaff5cb77b4754f260c8075dadd0c1194c7c663
Jørgen Vatle <Jorgen@WcAServices.net>
2023-11-15 22:30:26 +0200

    Move model exports into model index.

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,6 @@
-import ShopModel from './models/ShopModel';
-import CategoryModel from './models/CategoryModel';
-import CustomerBlacklistModel from './models/CustomerBlacklistModel';
-import IpnModel from './models/IpnModel';
-import OrderEventModel from './models/OrderEventModel';
-import OrderFeedbackModel from './models/OrderFeedbackModel';
-import OrderModel from './models/OrderModel';
-import ProductModel from './models/ProductModel';
-import ProductStockModel from './models/ProductStockModel';
-import UserModel from './models/UserModel';
-import GatewayModel from './models/GatewayModel';
 import ATShopServiceModels from './config';
-import ProductGroupModel from './models/ProductGroupModel';
-import OrderReplacementModel from './models/OrderReplacementModel';
-import CouponModel from './models/CouponModel';
+export * from './models';
 
 export {
-    CategoryModel,
-    GatewayModel,
-    CustomerBlacklistModel,
-    IpnModel,
-    OrderEventModel,
-    OrderReplacementModel,
-    OrderFeedbackModel,
-    OrderModel,
-    ProductModel,
-    ProductGroupModel,
-    ProductStockModel,
-    CouponModel,
-    ShopModel,
-    UserModel,
     ATShopServiceModels,
 }
---
+++ src/models/index.ts
@@ -0,0 +1,31 @@
+import ShopModel from './ShopModel';
+import CategoryModel from './CategoryModel';
+import CustomerBlacklistModel from './CustomerBlacklistModel';
+import IpnModel from './IpnModel';
+import OrderEventModel from './OrderEventModel';
+import OrderFeedbackModel from './OrderFeedbackModel';
+import OrderModel from './OrderModel';
+import ProductModel from './ProductModel';
+import ProductStockModel from './ProductStockModel';
+import UserModel from './UserModel';
+import GatewayModel from './GatewayModel';
+import ProductGroupModel from './ProductGroupModel';
+import OrderReplacementModel from './OrderReplacementModel';
+import CouponModel from './CouponModel';
+
+export {
+    CategoryModel,
+    GatewayModel,
+    CustomerBlacklistModel,
+    IpnModel,
+    OrderEventModel,
+    OrderReplacementModel,
+    OrderFeedbackModel,
+    OrderModel,
+    ProductModel,
+    ProductGroupModel,
+    ProductStockModel,
+    CouponModel,
+    ShopModel,
+    UserModel,
+}
